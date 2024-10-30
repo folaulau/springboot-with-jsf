c3dccaaaed45fb2e2237d5c9d43a07f77e6e7cb4
Folau Kaveinga <folaukaveinga@gmail.com>
2024-10-30 12:45:50 -0600

    setup

---
+++ src/main/java/com/folau/jsf/SpringbootWithJsfApplication.java
@@ -0,0 +1,13 @@
+package com.folau.jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithJsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folau/jsf/SpringbootWithJsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folau.jsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithJsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
